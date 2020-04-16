225fe17d461c951cd26c9bdc944104b86d5a7546
wushuai <wushuai@6estates.com>
2020-04-16 20:38:14 +0800

    重构页面表单校验方法

--- mycat-web-front/src/components/views/HouseCreate.vue
+++ mycat-web-front/src/components/views/HouseCreate.vue
@@ -1,20 +1,20 @@
 <template>
   <v-update-domain :saveact="comutil.FormInfo.ActRetainInputReset">
-    <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
+    <v-text-field v-model="name" :counter="10" :rules="[v => comutil.Validator.houseNameCheck(v)]" label="Name" required></v-text-field>
 
-    <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+    <v-text-field v-model="email" :rules="[v => comutil.Validator.emailCheck(v)]" label="E-mail" required></v-text-field>
 
     <v-select
       v-model="select"
       :items="items"
-      :rules="[v => !!v || 'Item is required']"
+      :rules="[v => comutil.Validator.itemCheck(v)]"
       label="Item"
       required
     ></v-select>
 
     <v-checkbox
       v-model="checkbox"
-      :rules="[v => !!v || 'You must agree to continue!']"
+      :rules="[v => comutil.Validator.agreeCheck(v)]"
       label="Do you agree?"
       required
     ></v-checkbox>
@@ -24,15 +24,7 @@
 export default {
   data: () => ({
     name: '',
-    nameRules: [
-      v => !!v || 'Name is required',
-      v => (v && v.length <= 10) || 'Name must be less than 10 characters'
-    ],
     email: '',
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
-    ],
     select: '',
     items: ['Item 1', 'Item 2', 'Item 3', 'Item 4'],
     checkbox: false,
